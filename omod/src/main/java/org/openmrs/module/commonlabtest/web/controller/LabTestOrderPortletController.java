d62dc5f380e47791a3ca97e4e1e9a5f7be466cff
Muhammad Ahmed <engrmahmed14@gmail.com>
2018-07-24 16:29:24 +0500

    portlet controller

---
+++ omod/src/main/java/org/openmrs/module/commonlabtest/web/controller/LabTestOrderPortletController.java
@@ -0,0 +1,96 @@
+package org.openmrs.module.commonlabtest.web.controller;
+
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.commonlabtest.LabTest;
+import org.openmrs.module.commonlabtest.api.impl.CommonLabTestServiceImpl;
+import org.openmrs.web.WebConstants;
+import org.openmrs.web.controller.PortletController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author engrmahmed14@gmail.com
+ */
+//@Controller
+//@RequestMapping("**/patientLabTests.portlet")
+public class LabTestOrderPortletController extends PortletController {
+	
+	/*	@Autowired
+		CommonLabTestService commonLabTestService;*/
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        Object uri = request.getAttribute("javax.servlet.include.servlet_path");
+        String portletPath = "";
+
+        portletPath = uri.toString();
+
+        // Allowable extensions are '' (no extension) and '.portlet'
+        if (portletPath.endsWith("portlet")) {
+            portletPath = portletPath.replace(".portlet", "");
+        } else if (portletPath.endsWith("jsp")) {
+            throw new ServletException(
+                    "Illegal extension used for portlet: '.jsp'. Allowable extensions are '' (no extension) and '.portlet'");
+        }
+
+        log.debug("Loading portlet: " + portletPath);
+        Map<String, Object> model = null;
+        {
+            HttpSession session = request.getSession();
+            String uniqueRequestId = (String) request.getAttribute(WebConstants.INIT_REQ_UNIQUE_ID);
+            String lastRequestId = (String) session.getAttribute(WebConstants.OPENMRS_PORTLET_LAST_REQ_ID);
+            if (uniqueRequestId.equals(lastRequestId)) {
+                model = (Map<String, Object>) session.getAttribute(WebConstants.OPENMRS_PORTLET_CACHED_MODEL);
+
+                // remove cached parameters
+                List<String> parameterKeys = (List<String>) model.get("parameterKeys");
+                if (parameterKeys != null) {
+                    for (String key : parameterKeys) {
+                        model.remove(key);
+                    }
+                }
+            }
+            if (model == null) {
+                log.debug("creating new portlet model");
+                model = new HashMap<String, Object>();
+                session.setAttribute(WebConstants.OPENMRS_PORTLET_LAST_REQ_ID, uniqueRequestId);
+                session.setAttribute(WebConstants.OPENMRS_PORTLET_CACHED_MODEL, model);
+            }
+        }
+
+        String patientId = request.getParameter("patientId");
+        System.out.println("Patient ID :: " + patientId);
+
+        populateModel(request, model);
+        return new ModelAndView(portletPath, "model", model);
+        //return super.handleRequest(request, response);
+    }
+
+    @Override
+    protected void populateModel(HttpServletRequest request, Map<String, Object> model) {
+
+        String patientId = request.getParameter("patientId");
+        System.out.println("Patient ID :: " + patientId);
+        if (patientId != null) {
+            int id = Integer.parseInt(patientId);
+            Patient patient = Context.getPatientService().getPatient(id);
+            if (patient != null) {
+                System.out.println("Patient  :: " + patient);
+                List<LabTest> testList = Context.getService(CommonLabTestServiceImpl.class).getLabTests(patient, false);
+                //List<LabTest> testList = commonLabTestService.getLabTests(patient, false);
+                model.put("testOrder", testList);
+            }
+        }
+
+    }
+}
