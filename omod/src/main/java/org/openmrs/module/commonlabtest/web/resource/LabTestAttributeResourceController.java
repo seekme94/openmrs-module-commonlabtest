05f1815d40ba26b93ed82cef67bedd825c7bb07a
shujaat.ali <shujaat.ali@ihsinformatics.com>
2018-09-06 11:27:58 +0500

    Resources

--- omod/src/main/java/org/openmrs/module/commonlabtest/web/resource/LabTestAttributeResourceController.java
+++ omod/src/main/java/org/openmrs/module/commonlabtest/web/resource/LabTestAttributeResourceController.java
@@ -16,79 +16,79 @@ import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceD
 import org.openmrs.module.webservices.rest.web.response.ResourceDoesNotSupportOperationException;
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
 
-@Resource(name = RestConstants.VERSION_1 + "/commonlab/labtestattribute", supportedClass = LabTestAttribute.class, supportedOpenmrsVersions = {"2.0.*,2.1.*"})
+@Resource(name = RestConstants.VERSION_1 + "/commonlab/labtestattribute", supportedClass = LabTestAttribute.class, supportedOpenmrsVersions = { "2.0.*,2.1.*" })
 public class LabTestAttributeResourceController extends DataDelegatingCrudResource<LabTestAttribute> {
-
-    /**
-     * Logger for this class
-     */
-    protected final Log log = LogFactory.getLog(getClass());
+	
+	/**
+	 * Logger for this class
+	 */
+	protected final Log log = LogFactory.getLog(getClass());
 	
 	/*	@Autowired
 		CommonLabTestService commonLabTestService;*/
-
-    private CommonLabTestService commonLabTestService = Context.getService(CommonLabTestService.class);
-
-    @Override
-    public LabTestAttribute getByUniqueId(String s) {
-
-        return commonLabTestService.getLabTestAttributeByUuid(s);
-    }
-
-    @Override
-    protected void delete(LabTestAttribute labTestAttribute, String s, RequestContext requestContext)
-            throws ResponseException {
-        commonLabTestService.voidLabTestAttribute(labTestAttribute, s);
-    }
-
-    @Override
-    public LabTestAttribute newDelegate() {
-        return new LabTestAttribute();
-    }
-
-    @Override
-    public LabTestAttribute save(LabTestAttribute labTestAttribute) {
-        return commonLabTestService.saveLabTestAttribute(labTestAttribute);
-    }
-
-    @Override
-    public void purge(LabTestAttribute labTestAttribute, RequestContext requestContext) throws ResponseException {
-        throw new ResourceDoesNotSupportOperationException();
-    }
-
-    @Override
-    public DelegatingResourceDescription getRepresentationDescription(Representation representation) {
-        if (representation instanceof DefaultRepresentation) {
-            DelegatingResourceDescription description = new DelegatingResourceDescription();
-            description.addProperty("uuid");
-            description.addProperty("labTestAttributeId");
-            description.addProperty("testOrderId");
-            description.addProperty("attributeTypeId");
-            description.addProperty("valueReference");
-
-            description.addSelfLink();
-            description.addLink("full", ".?v=" + RestConstants.REPRESENTATION_FULL);
-            return description;
-        } else if (representation instanceof FullRepresentation) {
-            DelegatingResourceDescription description = new DelegatingResourceDescription();
-            description.addProperty("uuid");
-            description.addProperty("labTestAttributeId");
-            description.addProperty("testOrderId");
-            description.addProperty("attributeTypeId");
-            description.addProperty("valueReference");
-
-            description.addProperty("creator");
-            description.addProperty("dateCreated");
-
-            description.addProperty("changedBy");
-            description.addProperty("dateChanged");
-
-            description.addProperty("voided");
-            description.addProperty("dateVoided");
-            description.addProperty("voidedBy");
-            description.addProperty("voidReason");
-            return description;
-        }
-        return null;
+	
+	private CommonLabTestService commonLabTestService = Context.getService(CommonLabTestService.class);
+	
+	@Override
+	public LabTestAttribute getByUniqueId(String s) {
+		
+		return commonLabTestService.getLabTestAttributeByUuid(s);
+	}
+	
+	@Override
+	protected void delete(LabTestAttribute labTestAttribute, String s, RequestContext requestContext)
+	        throws ResponseException {
+		commonLabTestService.voidLabTestAttribute(labTestAttribute, s);
+	}
+	
+	@Override
+	public LabTestAttribute newDelegate() {
+		return new LabTestAttribute();
+	}
+	
+	@Override
+	public LabTestAttribute save(LabTestAttribute labTestAttribute) {
+		return commonLabTestService.saveLabTestAttribute(labTestAttribute);
+	}
+	
+	@Override
+	public void purge(LabTestAttribute labTestAttribute, RequestContext requestContext) throws ResponseException {
+		throw new ResourceDoesNotSupportOperationException();
+	}
+	
+	@Override
+	public DelegatingResourceDescription getRepresentationDescription(Representation representation) {
+		if (representation instanceof DefaultRepresentation) {
+			DelegatingResourceDescription description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("labTestAttributeId");
+			description.addProperty("testOrderId");
+			description.addProperty("attributeTypeId");
+			description.addProperty("valueReference");
+			
+			description.addSelfLink();
+			description.addLink("full", ".?v=" + RestConstants.REPRESENTATION_FULL);
+			return description;
+		} else if (representation instanceof FullRepresentation) {
+			DelegatingResourceDescription description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("labTestAttributeId");
+			description.addProperty("testOrderId");
+			description.addProperty("attributeTypeId");
+			description.addProperty("valueReference");
+			
+			description.addProperty("creator");
+			description.addProperty("dateCreated");
+			
+			description.addProperty("changedBy");
+			description.addProperty("dateChanged");
+			
+			description.addProperty("voided");
+			description.addProperty("dateVoided");
+			description.addProperty("voidedBy");
+			description.addProperty("voidReason");
+			return description;
+		}
+		return null;
 	}
 }
